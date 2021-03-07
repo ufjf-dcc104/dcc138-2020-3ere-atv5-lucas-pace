d84473cf4954529bb68953f18640a7205e4a6e50
Lucas Pace <lucas.pace@hotmail.com.br>
2021-03-07 20:33:10 -0300

    usa construtor para criar opções de construção para a sprite

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+import Cena from './Cena.js'
+import Sprite from './Sprites.js';
 const canvas = document.querySelector("canvas");
-console.log(canvas)
+const cena1 = new Cena(canvas);
+const ctx = canvas.getContext("2d")
+cena1.desenhar()
+
+const pc = new Sprite();
+const pc1 = new Sprite({
+    x: 30,
+    y: 30,
+    w: 30,
+    h: 30,
+    color: "red"
+});
+
+pc.desenhar(ctx)
+pc1.desenhar(ctx)
