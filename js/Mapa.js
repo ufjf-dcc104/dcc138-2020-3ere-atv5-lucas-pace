688b7a022aaf409b1365ebfd78d15ba9133210ee
Lucas Pace <lucas.pace@hotmail.com.br>
2021-03-09 04:23:54 -0300

    simplificando desenho mapa

--- js/Mapa.js
+++ js/Mapa.js
@@ -1,5 +1,5 @@
 export default class Mapa {
-    constructor(linhas = 8, colunas = 12 , tamanho = 32) {
+    constructor(linhas = 8, colunas = 12, tamanho = 32) {
         this.LINHAS = linhas
         this.COLUNAS = colunas
         this.SIZE = tamanho
@@ -17,37 +17,35 @@ export default class Mapa {
     desenhar(ctx) {
         for (let l = 0; l < this.LINHAS; l++) {
             for (let c = 0; c < this.COLUNAS; c++) {
-                switch(this.tiles[l][c]){
+                switch (this.tiles[l][c]) {
                     case 1:
                         ctx.fillStyle = "grey"
-                        ctx.fillRect(c*this.SIZE, l*this.SIZE ,this.SIZE, this.SIZE)
+
                         ctx.lineWidht = 1;
                         ctx.strokeStyle = "black"
-                        ctx.strokeRect(c*this.SIZE, l*this.SIZE ,this.SIZE, this.SIZE)
+
                         break;
                     case 2:
                         ctx.fillStyle = "red"
-                        ctx.fillRect(c*this.SIZE, l*this.SIZE ,this.SIZE, this.SIZE)
                         ctx.lineWidht = 1;
                         ctx.strokeStyle = "orange"
-                        ctx.strokeRect(c*this.SIZE, l*this.SIZE ,this.SIZE, this.SIZE)
                         break;
                     default:
                         ctx.fillStyle = "black"
-                        ctx.fillRect(c*this.SIZE, l*this.SIZE ,this.SIZE, this.SIZE)
                         ctx.lineWidht = 1;
                         ctx.strokeStyle = "grey"
-                        ctx.strokeRect(c*this.SIZE, l*this.SIZE ,this.SIZE, this.SIZE)
                         break;
-                }
+                    }
+                    ctx.fillRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE)
+                    ctx.strokeRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE)
             }
 
         }
     }
 
-    carregaMapa(modelo){
+    carregaMapa(modelo) {
         this.LINHAS = modelo.length
-        this.COLUNAS = modelo[0]?.length ?? 0
+        this.COLUNAS = modelo[0] ?.length ?? 0
         this.tiles = [];
         for (let l = 0; l < this.LINHAS; l++) {
             this.tiles[l] = []
