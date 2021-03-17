3929cf900cecea8e3cc6022a1c506e258444dcaf
Lucas Pace <lucas.pace@hotmail.com.br>
2021-03-16 23:18:16 -0300

    Novas opções no configurarTeclado e comentando o summon

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import Mapa from './Mapa.js';
 import Mixer from './Mixer.js';
 import Sprite from './Sprites.js';
 import modeloMapa1 from '../maps/mapa1.js'
+import ImputManager from './InputManager.js'
 
 import SpriteSummon from './SpriteSummon.js';
 import SpriteSummonInterval from './SpriteSummonInterval.js';
@@ -11,44 +12,78 @@ import SpriteSummonInterval from './SpriteSummonInterval.js';
 const mapX = 15;
 const mapY = 15;
 
+const input = new ImputManager()
 const mixer = new Mixer(10)
 const assets = new AssetManager(mixer);
 const canvas = document.querySelector("canvas");
 canvas.width = mapX * 32
 canvas.height = mapY * 32
+
+//teclado
+input.configurarTeclado({
+    "ArrowLeft": "MOVE_ESQUERDA",
+    "ArrowRight": "MOVE_DIREITA",
+    "ArrowUp": "MOVE_CIMA",
+    "ArrowDown": "MOVE_BAIXO",
+})
+
+
 const cena1 = new Cena(canvas, assets);
 const ctx = canvas.getContext("2d")
 const mapa1 = new Mapa(mapX, mapY, 32, assets)
+
 mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1)
 
 //const summon = new SpriteSummon(1, cena1, canvas.width, canvas.height)
-const summonInterval = new SpriteSummonInterval(cena1, 4000, canvas.width, canvas.height)
-cena1.summonSprite = true; //boleano para habilitar o summon de sprite na cena
-summonInterval.startSummon()
+// const summonInterval = new SpriteSummonInterval(cena1, 4000, canvas.width, canvas.height)
+// cena1.summonSprite = true; //boleano para habilitar o summon de sprite na cena
+// summonInterval.startSummon()
+
 
 //new SpriteSummon(100, cena1,canvas.width,canvas.height);
 
-// const pc = new Sprite({
-//     vx: 0,
-//     vy: 10,
-//     x: 300,
-//     y: 50
-// });
-// const pc1 = new Sprite({
-//     vx: 0,
-//     x: 100,
-//     y: 50,
-//     color: "green"
-// });
-// cena1.adicionar(pc)
-// cena1.adicionar(pc1)
-// cena1.adicionar(new Sprite({
-//     vx: 40,
-//     x: 100,
-//     y: 110,
-//     color: "red"
-// }))
+const pc = new Sprite({
+    vx: 0,
+    vy: 10,
+    x: 300,
+    y: 50
+});
+pc.controlar = function(dt){
+    if(input.comandos.get("MOVE_ESQUERDA")){
+        this.vx = -50
+    }
+    else if(input.comandos.get("MOVE_DIREITA")){
+        this.vx = 50
+    }
+    else {
+        this.vx = 0
+    }
+    if(input.comandos.get("MOVE_CIMA")){
+        this.vy = -50
+    }
+    else if(input.comandos.get("MOVE_BAIXO")){
+        this.vy = 50
+    }
+    else {
+        this.vy = 0
+    }
+}
+
+const pc1 = new Sprite({
+    vx: 0,
+    x: 100,
+    y: 50,
+    color: "silver"
+});
+cena1.adicionar(pc)
+cena1.adicionar(pc1)
+cena1.adicionar(new Sprite({
+    vx: 40,
+    x: 100,
+    y: 110,
+    color: "red"
+}))
 
 
 cena1.iniciar()
